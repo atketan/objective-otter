10a26be5b743c3169739c19b39115b26cedbefff
Ketan Bhavsar <mr.ketanbhavsar@gmail.com>
2024-06-18 12:03:35 +0530

    refactor: Remove commented code and unused imports in ProjectsHomeScreen

--- lib/presentation/projects_home/projects_home_screen.dart
+++ lib/presentation/projects_home/projects_home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:objective_otter/presentation/projects_home/models/project_status.dart';
 import 'package:objective_otter/presentation/projects_home/models/projects_card_details.dart';
 import 'package:objective_otter/presentation/projects_home/widgets/projects_card_widget.dart';
 
@@ -12,86 +11,86 @@ class ProjectsScreen extends StatefulWidget {
 }
 
 class ProjectsScreenState extends State<ProjectsScreen> {
-  List<ProjectsCardDetails> projectCards = [
-    ProjectsCardDetails(
-      projectId: "Oqifpvaah0Q2MKMC6jKI",
-      projectName: "Project Bumblebee",
-      projectDescription:
-          "NAF Website Modernisation through People, Process & Technology Transformation Strategy",
-      projectIcon: Icon(
-        Icons.rocket_launch_sharp,
-        color: Colors.yellow[700],
-        size: 64,
-      ),
-      projectTags: [
-        "NAF",
-        "Accelerate",
-        "Website",
-        "PPT Strategy",
-      ],
-      projectStatus: ProjectStatus.review,
-      primaryOwnerName: "TBD",
-    ),
-    ProjectsCardDetails(
-      projectId: "Oqifpvaah0Q2MKMC6jKI",
-      projectName: "NAF Concierge",
-      projectDescription: "LLM-powered Support ChatBots Project",
-      projectIcon: const Icon(
-        Icons.android_sharp,
-        color: Colors.blue,
-        size: 64,
-      ),
-      projectTags: [
-        "NAF",
-        "LLM",
-        "Dial-a-Doughminator",
-        "Concierge",
-        "Support",
-      ],
-      projectStatus: ProjectStatus.review,
-      primaryOwnerName: "TBD",
-    ),
-    ProjectsCardDetails(
-      projectId: "Oqifpvaah0Q2MKMC6jKI",
-      projectName: "Project X",
-      projectDescription:
-          "Headless CMS Test Suite - to support Bumblebee testing and sign-off",
-      projectIcon: const Icon(
-        Icons.factory_sharp,
-        color: Colors.black,
-        size: 64,
-      ),
-      projectTags: [
-        "CMS",
-        "Test Suite",
-        "Headless",
-        "API-driven",
-        "Bumblebee",
-      ],
-      projectStatus: ProjectStatus.planned,
-      primaryOwnerName: "TBD",
-    ),
-    ProjectsCardDetails(
-      projectId: "Oqifpvaah0Q2MKMC6jKI",
-      projectName: "Short Circuit",
-      projectDescription:
-          "Simplify PIM consumption architecture; explore direct integration and PoC alongside Bumblebee",
-      projectIcon: const Icon(
-        Icons.shortcut_sharp,
-        color: Colors.deepOrange,
-        size: 64,
-      ),
-      projectTags: [
-        "PIM",
-        "EA&S",
-        "No Code",
-        "Low Code",
-        "Bumblebee",
-      ],
-      projectStatus: ProjectStatus.ongoing,
-      primaryOwnerName: "TBD",
-    ),
-  ];
+  // List<ProjectsCardDetails> projectCards = [
+  //   ProjectsCardDetails(
+  //     projectId: "Oqifpvaah0Q2MKMC6jKI",
+  //     projectName: "Project Bumblebee",
+  //     projectDescription:
+  //         "NAF Website Modernisation through People, Process & Technology Transformation Strategy",
+  //     projectIcon: Icon(
+  //       Icons.rocket_launch_sharp,
+  //       color: Colors.yellow[700],
+  //       size: 64,
+  //     ),
+  //     projectTags: [
+  //       "NAF",
+  //       "Accelerate",
+  //       "Website",
+  //       "PPT Strategy",
+  //     ],
+  //     projectStatus: ProjectStatus.review,
+  //     primaryOwnerName: "TBD",
+  //   ),
+  //   ProjectsCardDetails(
+  //     projectId: "Oqifpvaah0Q2MKMC6jKI",
+  //     projectName: "NAF Concierge",
+  //     projectDescription: "LLM-powered Support ChatBots Project",
+  //     projectIcon: const Icon(
+  //       Icons.android_sharp,
+  //       color: Colors.blue,
+  //       size: 64,
+  //     ),
+  //     projectTags: [
+  //       "NAF",
+  //       "LLM",
+  //       "Dial-a-Doughminator",
+  //       "Concierge",
+  //       "Support",
+  //     ],
+  //     projectStatus: ProjectStatus.review,
+  //     primaryOwnerName: "TBD",
+  //   ),
+  //   ProjectsCardDetails(
+  //     projectId: "Oqifpvaah0Q2MKMC6jKI",
+  //     projectName: "Project X",
+  //     projectDescription:
+  //         "Headless CMS Test Suite - to support Bumblebee testing and sign-off",
+  //     projectIcon: const Icon(
+  //       Icons.factory_sharp,
+  //       color: Colors.black,
+  //       size: 64,
+  //     ),
+  //     projectTags: [
+  //       "CMS",
+  //       "Test Suite",
+  //       "Headless",
+  //       "API-driven",
+  //       "Bumblebee",
+  //     ],
+  //     projectStatus: ProjectStatus.planned,
+  //     primaryOwnerName: "TBD",
+  //   ),
+  //   ProjectsCardDetails(
+  //     projectId: "Oqifpvaah0Q2MKMC6jKI",
+  //     projectName: "Short Circuit",
+  //     projectDescription:
+  //         "Simplify PIM consumption architecture; explore direct integration and PoC alongside Bumblebee",
+  //     projectIcon: const Icon(
+  //       Icons.shortcut_sharp,
+  //       color: Colors.deepOrange,
+  //       size: 64,
+  //     ),
+  //     projectTags: [
+  //       "PIM",
+  //       "EA&S",
+  //       "No Code",
+  //       "Low Code",
+  //       "Bumblebee",
+  //     ],
+  //     projectStatus: ProjectStatus.ongoing,
+  //     primaryOwnerName: "TBD",
+  //   ),
+  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -128,7 +127,6 @@ class ProjectsScreenState extends State<ProjectsScreen> {
                     Map<String, dynamic> data =
                         document.data()! as Map<String, dynamic>;
                     // Assuming 'data' contains all the necessary fields for your ProjectsCardWidget
-                    print(data.toString());
                     return SizedBox(
                       width: 500,
                       child: ProjectsCardWidget(
